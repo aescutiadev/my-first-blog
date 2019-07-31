4c8992b37b047ac6e626b1c4590ddd08d13abbdb
noa-index <abyss.floyd@gmail.com>
2019-07-31 18:40:00 -0500

    version 1.1.2

--- inicio/views.py
+++ inicio/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import PostForm
 
-# Create your views here.
+
 def post_list(request):
-    return render(request, 'inicio/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'inicio/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'inicio/post_detail.html', {'post': post})
+
+def post_new(request):
+    if (request.method == "POST"): # Si el metodo es POST entonces:
+        form = PostForm(request.POST) # Hace el request
+        if (form.is_valid()): # Valida si no hay errores
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('inicio.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'inicio/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method == "POST":
+		form = PostForm(request.POST, instance=post)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm(instance=post)
+	return render(request, 'inicio/post_edit.html', {'form': form})
